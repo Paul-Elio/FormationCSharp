61694289a374d2c2d8aff9522a59b732fc3ab0b9
Formation <formation@intm.com>
2024-01-02 14:05:00 +0100

    full push

--- test-intm1/Program.cs
+++ test-intm1/Program.cs
@@ -10,6 +10,14 @@ namespace test_intm1
     {
         static void Main(string[] args)
         {
+            string txt1 = "hello world";
+            Console.WriteLine(txt1);
+            txt1 = "ho hi mark";
+            string txt2 = "  heyyy";
+            Console.WriteLine(txt1+txt2);
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey()
+                ;
         }
     }
 }
