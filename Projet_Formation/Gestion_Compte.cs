b4954965018e2f00f372e54448e6c7df08dcf78d
Formation <formation@intm.com>
2024-01-11 11:55:14 +0100

    push mi-partie 2

--- Projet_Formation/Gestion_Compte.cs
+++ Projet_Formation/Gestion_Compte.cs
@@ -11,11 +11,12 @@ namespace Projet_Formation
 
     public class Gestion_Compte
     {
-
+        public List<Operation> Historique_ope = new List<Operation>();
         public Dictionary<uint, Compte> Banque = new Dictionary<uint, Compte>();
         public List<Transaction> Historique_Banque = new List<Transaction>();
         public Dictionary<uint, Gestionnaire> Clients = new Dictionary<uint, Gestionnaire>();
         private const int Max_Retrait = 1000;
+        private const int Max_week = 2000;
         public int nb_compte = 0;
         public int nb_tr_ok = 0;
         public int nb_tr_ko = 0;
@@ -46,7 +47,7 @@ namespace Projet_Formation
             }
             return false;
         }
-        public bool Create_client(uint num_cli, bool type)
+        public bool Create_client(uint num_cli, bool type, int tr_max)
         {
             if (!Existe_client(num_cli))
             {
@@ -54,6 +55,7 @@ namespace Projet_Formation
                 cli.Type = type;
                 cli.Comptes = new List<uint>();
                 cli.Frai_banquaires = 0;
+                cli.tr_max = tr_max;
                 Clients[num_cli] = cli;
                 return true;
             }
@@ -67,6 +69,7 @@ namespace Projet_Formation
                 cpt.Solde = solde;
                 cpt.Historique = new List<decimal>();
                 cpt.Date_ouv = date;
+                cpt.Histo_transac = new List<DateTime>();
                 cpt.Actif = true;
                 Banque[num_cpt] = cpt;
                 return true;
@@ -127,17 +130,18 @@ namespace Projet_Formation
             }
             return false;
         }
-        public bool Retrait(uint num_cpt, decimal montant)
+        public bool Retrait(uint num_cpt, decimal montant, DateTime date)
         {
             if (montant > 0)
             {
                 if (Existe_compte(num_cpt) && Banque[num_cpt].Actif)
                 {
-                    if (Banque[num_cpt].Solde >= montant && !Plafond_atteint(num_cpt, montant))
+                    if (Banque[num_cpt].Solde >= montant && !Plafond1_atteint(num_cpt, montant) && !Plafond2_atteint(num_cpt, montant, date))
                     {
                         Compte cpt = Banque[num_cpt];
                         cpt.Solde -= montant;
                         cpt.Historique.Add(montant);
+                        cpt.Histo_transac.Add(date);
                         Banque[num_cpt] = cpt;
                         return true;
                     }
@@ -145,14 +149,22 @@ namespace Projet_Formation
             }
             return false;
         }
-        public bool Plafond_atteint(uint num_cpt, decimal montant)
+        public bool Plafond1_atteint(uint num_cpt, decimal montant)
         {
+            int tr_max = 0;
+            foreach(uint cli in Clients.Keys)
+            {
+                if (Clients[cli].Comptes.Any(x => x == num_cpt))
+                {
+                    tr_max = Clients[cli].tr_max;
+                }
+            }
             decimal sum = montant;
             Compte cpt = Banque[num_cpt];
             int nb_tr = cpt.Historique.Count;
-            if (nb_tr >= 9)
+            if (nb_tr >= tr_max-1)
             {
-                for (int i = 1; i <= 9; i++)
+                for (int i = 1; i <= tr_max; i++)
                 {
                     sum += Banque[num_cpt].Historique[nb_tr - i];
                 }
@@ -166,24 +178,45 @@ namespace Projet_Formation
             }
             return (sum > Max_Retrait);
         }
-        public bool Virement(decimal montant, uint expe, uint dest)
+        public bool Plafond2_atteint(uint num_cpt, decimal montant, DateTime date)
+        {
+            decimal sum = montant;
+            Compte cpt = Banque[num_cpt];
+            int i= cpt.Histo_transac.Count-1;
+            TimeSpan week = TimeSpan.FromDays(7);
+            while (date - cpt.Histo_transac[i] <= week)
+            {
+                sum += cpt.Historique[i];
+                i--;
+            }
+            return (sum > Max_week);
+        }
+        public bool Virement(decimal montant, uint expe, uint dest, DateTime date)
         {
             if (Existe_compte(expe) && Existe_compte(dest))
             {
-                if (Retrait(expe, montant))
+                if (Retrait(expe, montant, date))
                 {
-                    return Depot(dest, montant); ;
+                    decimal frai_banquaire = 0;
+                    foreach (uint client_id in Clients.Keys)
+                    {
+                        if (Clients[client_id].Comptes.Any(x => x == expe))
+                        {
+                            // si le compte destinataire est chez le même client les frai banquaires sont de 0;
+                            // sinon on regarde son type est si c'est true c'est un Particulier et sinon c'est une entreprise;
+                            frai_banquaire = Clients[client_id].Comptes.Any(x => x == dest) ? 0 : Clients[client_id].Type ? (decimal)0.01 * montant : 10;
+                            Gestionnaire cli = Clients[client_id];
+                            cli.Frai_banquaires += frai_banquaire;
+                            Clients[client_id] = cli;
+                        }
+                    }
+                    return Depot(dest, montant-frai_banquaire); 
                 }
             }
             return false;
         }
-        public void traitement_transaction(uint trid, decimal montant, uint expe, uint dest)
+        public void Traitement_transaction(Transaction tr)
         {
-            Transaction tr = new Transaction();
-            tr.Transaction_ID = trid;
-            tr.Montant = montant;
-            tr.Expediteur = expe;
-            tr.Destinataire = dest;
             tr.Statut = false;
             if (Existe_transaction(tr.Transaction_ID))
             {
@@ -195,13 +228,99 @@ namespace Projet_Formation
             }
             else if (tr.Destinataire == 0)
             {
-                tr.Statut = Retrait(tr.Expediteur, tr.Montant);
+                tr.Statut = Retrait(tr.Expediteur, tr.Montant, tr.Date );
             }
             else
             {
-                tr.Statut = Virement(tr.Montant, tr.Expediteur, tr.Destinataire);
+                tr.Statut = Virement(tr.Montant, tr.Expediteur, tr.Destinataire, tr.Date);
             }
             Historique_Banque.Add(tr);
+            return;
+        }
+        public void Traitement_operation(Operation ope)
+        {
+            if (ope.Sortie == 0)
+            {
+                ope.Statut =Add_compte_client(ope.Entree, ope.Num_cpt, ope.Date_ope, ope.Solde_init);
+            }
+            else if (ope.Entree == 0)
+            {
+                ope.Statut = Remove_compte_client(ope.Sortie, ope.Num_cpt);
+            }
+            else
+            {
+                ope.Statut = Swap_compte_clients(ope.Entree, ope.Sortie, ope.Num_cpt);
+            }
+            Historique_ope.Add(ope);
+            return;
+        }
+        public void Chargement_Gestionnaire(string gestionnaire)
+        {
+            string[] gestion = File.ReadAllLines(gestionnaire);
+            foreach (string line in gestion)
+            {
+                try
+                {
+                    string[] elem = line.Split(';');
+                    uint gest_id;
+                    uint.TryParse(elem[0], out gest_id);
+                    int tr_max;
+                    int.TryParse(elem[2], out tr_max);
+                    bool type = true;
+                    if (elem[1]== "Particulier" || elem[1] == "Entreprise")
+                    {
+                        type = elem[1] == "Particulier";
+                    }
+                   else
+                    {
+                        Console.WriteLine($"Compte : {line} invalide !");
+                        continue;
+                    }
+
+                    Create_client(gest_id, type, tr_max);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Compte : {line} invalide !");
+                }
+            }
+            return;
+        }
+        public bool Valide_line_operation(string[] operation, ref Operation ope)
+        {
+            return false;
+        }
+        public bool Valide_line_transaction(string[] transaction, ref Transaction tr)
+        {
+            return false;
+        }
+        public void Gestion(string gestionnaire, string transactions, string operations, string stat_ope, string stat_tr, string cr)
+        {
+            Chargement_Gestionnaire(gestionnaire);
+            StreamReader transaction = new StreamReader(transactions);
+            StreamReader operation = new StreamReader(operations);
+            string[] ope = operation.ReadLine().Split(';');
+            string[] tr = transaction.ReadLine().Split(';');
+            do
+            {
+                Operation opera = new Operation();
+                Transaction transac = new Transaction();
+                while (!Valide_line_operation(ope, ref opera) && ope != null) ope = operation.ReadLine().Split(';');
+                while (!Valide_line_transaction(tr, ref transac) && tr != null) tr = transaction.ReadLine().Split(';');
+                if (opera.Date_ope <= transac.Date)
+                {
+                    Traitement_operation(opera);
+                    ope = operation.ReadLine().Split(';');
+                }
+                else
+                {
+                    Traitement_transaction(transac);
+                    tr = transaction.ReadLine().Split(';');
+                }
+            } while (ope != null && tr != null );
+            Compte_rendu_ope(stat_ope);
+            Compte_rendu_transac(stat_tr);
+            Compte_rendu(cr);
             return;
         }
         public void Compte_rendu(string cr)
@@ -215,40 +334,14 @@ namespace Projet_Formation
             File.WriteAllLines(cr, compte_rendu.ToArray());
             return;
         }
-        /** Chargement_Gestionnaire
-        public void Chargement_Gestionnaire(string gestionnaire)
+        public void Compte_rendu_ope(string stat_ope)
         {
-            string[] comptes = File.ReadAllLines(gestionnaire);
-            foreach (string line in comptes)
-            {
-                try
-                {
-                    string[] elem = line.Replace('.', ',').Split(';');
-                    uint num_cpt;
-                    uint.TryParse(elem[0], out num_cpt);
-                    decimal solde;
-                    if (elem[1] == "")
-                    {
-                        solde = 0;
-                    }
-                    else if (!decimal.TryParse(elem[1], out solde) || solde < 0)
-                    {
-                        Console.WriteLine($"Compte {line} invalide !");
-                        continue;
-                    }
-                    Create_compte(num_cpt, solde);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine($"Compte : {line} invalide !");
-                }
-            }
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Etat des comptes avant traitement :");
-            Display_comptes();
-            return;
+
         }
-        /**/
+        public void Compte_rendu_transac(string stat_tr)
+        {
+
+        }
         public void Display_comptes()
         {
             foreach (KeyValuePair<uint, Compte> compte in Banque)
@@ -257,7 +350,5 @@ namespace Projet_Formation
             }
             return;
         }
-
-
     }
 }
--- Projet_Formation/Gestionnaire.cs
+++ Projet_Formation/Gestionnaire.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Projet_Formation
 {
+    public struct Operation
+    {
+        public uint Num_cpt { get; set; }
+        public DateTime Date_ope { get; set; }
+        public decimal Solde_init { get; set; }
+        public uint Entree { get; set; }
+        public uint Sortie { get; set; }
+        public bool Statut { get; set; }
+    }
     public struct Gestionnaire
     {
         public bool Type { get; set; } // true = Particulier, False = Entreprise
         public List<uint> Comptes { get; set; }
         public decimal Frai_banquaires { get; set; }
+        public int tr_max { get; set; }
         public int Code_ope { get; set; } // Code 1 : Création de compte
                                           // Code 2 : Cloture de compte
                                           // Code 3 : Cession d'un compte =   Réception de compte 
@@ -22,12 +32,14 @@ namespace Projet_Formation
         public bool Statut { get; set; }
         public uint Expediteur { get; set; }
         public uint Destinataire { get; set; }
+        public DateTime Date { get; set; }
     }
 
     public struct Compte
     {
         public decimal Solde { get; set; }
         public List<decimal> Historique { get; set; }
+        public List<DateTime> Histo_transac { get; set; }
         public DateTime Date_ouv { get; set; }
         public bool Actif { get; set; }
     }
